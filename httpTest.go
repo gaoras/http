185b6e94ee282aa9e2b3622a559be08647282cf1
Yu <imok221@yahoo.co.jp>
2019-09-01 19:11:15 +0900

    jsonの読み込み作成

--- httpTest.go
+++ httpTest.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
 )
 
-func main() {
-	httpGetUsingHeader()
+//Data ..
+type Data struct {
+	Args    string `json:"args"`
+	Headers string `json:"headers"`
+	Origin  string `json:"origin"`
+	URL     string `json:"url"`
 }
 
-func httpGetUsingHeader() {
+func main() {
+	header := httpGetUsingHeader()
+	jsonTest(header)
+}
+
+func makeJSON() {
+	data := Data{Args: "123", Headers: "Accept", Origin: "12.34.56.789, 12.34.56.789", URL: "https://httpbin.org/get"}
+	jsonbytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("error")
+	}
+	fmt.Println(string(jsonbytes))
+
+}
+
+func jsonTest(data []byte) {
+	// JSONデコード
+	var dst Data
+	//var dst interface{}
+	if err := json.Unmarshal(data, &dst); err != nil {
+		log.Fatal(err)
+	}
+	// デコードしたデータを表示
+	fmt.Println(dst)
+}
+
+func httpGetUsingHeader() []byte {
 	uri := "https://httpbin.org/get"
 	q := url.Values{}
 	q.Add("query", "1234")
@@ -25,24 +55,13 @@ func httpGetUsingHeader() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	robots, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", robots)
-	str := string(robots)
-	spStr := strings.Split(str, "\n")
-	fmt.Println(spStr)
-	for index := 0; index < len(spStr); index++ {
-		n := strings.Index(spStr[index], "Authorization")
-		if n > 0 {
-			target := spStr[index][n:]
-			fmt.Println(target)
-			break
-		}
-	}
-	//	fmt.Println(n)
+	fmt.Printf("%s", body)
+	return body
 }
 
 func simpleGet() {
@@ -50,10 +69,10 @@ func simpleGet() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	robots, err := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s", robots)
+	fmt.Printf("%s", body)
 }
